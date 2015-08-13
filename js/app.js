b1c7d22d0b1005e3c8a4761bd0071049b0f1a3f3
Elcarim <elcarim5efil@gmail.com>
2015-08-13 14:15:42 +0800

    update info
    update info

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ var translations_zh = {
 		INFO:'东南大学 自动化学院',
 		gitrepo: 'https://github.com/elcarim5efil/repositories',
 		INTRO: '<p>勤奋刻苦，善于钻研，自律严谨，有探索精神，勇于尝试新技术，能够独立完成开发，也乐意参与团队协作相互学习。</p>'
-			+ '<p>自从尝试Web应用开发后一发不可收拾，爱上了JavaScript以及Node。鉴于自身爱好游戏，因此以JavaScript和Html5为基础，尝试实现一些小游戏，如2048、FlappyBird、DumbSoccer。虽然作品不多，但尝试过原生Canvas、matter.js、cocos2d-html5、chipmunk等开发工具，并有少许心得。</p>'
-			+ '<p>曾经尝试为Chrome浏览器开发扩展应用，目前已实现用于登录密码记录和视频弹窗的简单扩展应用，源码请见<a href="http://www.github.com/elcarim5efil" target="_blank">github</a>。</p>'
+			+ '<p>自从尝试Web应用开发后一发不可收拾，爱上了JavaScript和Web开发。鉴于自身爱好游戏，因此以JavaScript和Html5为基础，尝试实现一些小游戏，如2048、FlappyBird、DumbSoccer。虽然作品不多，但尝试过原生Canvas、matter.js、cocos2d-html5、chipmunk等开发工具，并有少许心得。</p>'
+			+ '<p>曾经尝试为Chrome浏览器开发扩展应用，目前已实现用于登录密码记录、视频弹窗以及车辆违章记录查询的扩展应用，源码请见<a href="http://www.github.com/elcarim5efil/ChromeExtensions" target="_blank">github</a>。</p>'
 			+ '<p>本页面使用AngularJS及Bootstrap进行开发，Blog模块使用了Jekyll，评论系统使用“多说”进行托管。</p>',
 	},
 	'MENU':{

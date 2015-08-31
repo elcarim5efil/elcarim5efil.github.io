7b39ec95144ceacaae59b43e8e25f7005fbe2c10
Elcarim <elcarim5efil@gmail.com>
2015-08-31 19:29:29 +0800

    bug fixed

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ var translations_zh = {
 		INTRO: '<p>勤奋刻苦，善于钻研，自律严谨，有探索精神，勇于尝试新技术，能够独立完成开发，也乐意参与团队协作相互学习。</p>'
 			+ '<p>自从尝试Web应用开发后一发不可收拾，爱上了JavaScript和Web开发。鉴于自身爱好游戏，因此以JavaScript和Html5为基础，尝试实现一些小游戏，如2048、FlappyBird、DumbSoccer。虽然作品不多，但尝试过原生Canvas、matter.js、cocos2d-html5、chipmunk等开发工具，并有少许心得。</p>'
 			+ '<p>曾经尝试为Chrome浏览器开发扩展应用，目前已实现用于登录密码记录、视频弹窗以及车辆违章记录查询的扩展应用，源码请见<a href="http://www.github.com/elcarim5efil/ChromeExtensions" target="_blank">github</a>。</p>'
-			+ '<p>本页面使用AngularJS及Bootstrap进行开发，Blog模块使用了Jekyll，评论系统使用“多说”进行托管。</p>',
-			+ '<p>近期正在开展一个基于纯CSS3实现的Material Design 风格的主题样式表。</p>',
+			+ '<p>本页面使用AngularJS及Bootstrap进行开发，Blog模块使用了Jekyll，评论系统使用“多说”进行托管。</p>'
+			+ '<p>近期正在开展一个基于纯CSS3实现的Material Design 风格的主题样式表。</p>'
 	},
 	'MENU':{
 		'PROFILE': '个人资料',
@@ -28,7 +28,8 @@ var translations_en = {
 			+ '<p>I love gaming and I\'ve tried to make some games based on JavaScript and HTML5, for instance, 2048, FlappyBird, DumbSoccer. There\'re not much of them, but I enjoy the process and the effort of tring new technologies, like Canvas, matter.js, cocos2d-html5, chipmunk. </p>'
 			+ '<p>Besides, I\'ve tried to develop some plugins for Chrome. So far, I\'ve develop a video pop out plugin and a auto account information fill in plugin. You can check the source code form my <a href="http://www.github.com/elcarim5efil" target="_blank">github</a>.</p>'
 			+ '<p>This page is developed on my own, using AngularJS and Bootstrap. The Blog module is based on Jekyll with a comment system named "DuoShuo".</p>'
-			+ '<p>For the people who cannot read Chinese, I\'d like to apologize for my poor English and lack of English translations. I would be glad to translate all my posts to English, but it could take lots of my time. So, if you are interested in any of the non-translated posts, please let me know, I will translate it ASAP.</p>',
+			+ '<p>For the people who cannot read Chinese, I\'d like to apologize for my poor English and lack of English translations. I would be glad to translate all my posts to English, but it could take lots of my time. So, if you are interested in any of the non-translated posts, please let me know, I will translate it ASAP.</p>'
+			+ '<p>Recently, I\'ve been working on a pure CSS3 project implementing the Material Design.</p>'
 	},
 	'MENU':{
 		'PROFILE': 'PROFILE',
